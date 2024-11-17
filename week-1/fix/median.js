49b16a506cddf95d770b09d0227fda7f83e6df77
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2024-11-17 19:13:03 +0100

    Tested and Fixed Median function

--- week-1/fix/median.js
+++ week-1/fix/median.js
@@ -3,9 +3,15 @@
 // If you're in the week-1 directory, you can run npm test -- fix to run the tests in the fix directory
 
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+  const listLength = list.length;
+  const middleIndex = Math.floor(listLength / 2);
+  const medianForOdd = list[middleIndex];
+  const sumOfEvenSet = list[middleIndex] + list[middleIndex - 1];
+  const averageEven = sumOfEvenSet / 2;
+
+  if (listLength % 2 === 0) {
+    return averageEven;
+  } else return medianForOdd;
 }
 
 module.exports = calculateMedian;
