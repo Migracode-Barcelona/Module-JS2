a1a2ab936829bd0b76bdd16afebe05bd37fa0cf9
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2024-11-18 16:19:31 +0100

    find function Tested and approved

--- week-1/refactor/find.test.js
+++ week-1/refactor/find.test.js
@@ -3,22 +3,22 @@
 const find = require("./find.js");
 
 test("returns index when target is in array", () => {
-  const currentOutput = find(["a", "b", "c", "d"], "c");
-  const targetOutput = 2;
+  const currentOutput = find(["a", "b", "o", "d", "i", "j", "f", "c"], "c");
+  const targetOutput = 7;
 
   expect(currentOutput).toBe(targetOutput);
 });
 
 test("returns -1 when target not in array", () => {
-  const currentOutput = find([1, 2, 3, 4], "a");
+  const currentOutput = find([1, 2, 3, 4], 9);
   const targetOutput = -1;
 
   expect(currentOutput).toBe(targetOutput);
 });
 
 test("returns index of first match", () => {
-  const currentOutput = find([1, 2, 2, 3], 2);
-  const targetOutput = 1;
+  const currentOutput = find([3, 2, 2, 3, 1, 6, 2], 1);
+  const targetOutput = 4;
 
   expect(currentOutput).toBe(targetOutput);
 });
@@ -31,8 +31,8 @@ test("returns -1 for empty array", () => {
 });
 
 test("searches for null", () => {
-  const currentOutput = find(["b", "z", null, "a"], null);
-  const targetOutput = 2;
+  const currentOutput = find(["b", "z", "a", "l", "t", null], null);
+  const targetOutput = 5;
 
   expect(currentOutput).toBe(targetOutput);
 });
