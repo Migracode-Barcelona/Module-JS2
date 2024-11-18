59d09ac285e412f460256d8c65c295442020449f
Ly-Ly Janusz <153077816+lylyjan@users.noreply.github.com>
2024-11-18 19:06:19 +0100

    assignment 18Nov

--- week-2/debug/address.js
+++ week-2/debug/address.js
@@ -4,6 +4,9 @@
 // but it isn't working...
 // Fix anything that isn't working
 
+// Answer: what is happening here is that we are trying to act as if the object was an array, but instead we should be using 
+// dot notation with a key
+
 const address = {
   houseNumber: 42,
   street: "Imaginary Road",
@@ -12,4 +15,4 @@ const address = {
   postcode: "XYZ 123",
 };
 
-console.log(`My house number is ${address[0]}`);
+console.log(`My house number is ${address.houseNumber}`);
