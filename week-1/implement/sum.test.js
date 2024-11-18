6b2adc6f2f708f0a96f8dbe4e2e7a738626794b6
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2024-11-18 15:47:32 +0100

    Sum function implemented and tested

--- week-1/implement/sum.test.js
+++ week-1/implement/sum.test.js
@@ -6,6 +6,7 @@ E.g. sum([10, 20, 30]), target output: 60
 E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical elements)
 */
 
+const sum = require("./sum");
 // Acceptance Criteria:
 
 // Given an empty array
@@ -27,3 +28,22 @@ E.g. sum(['hey', 10, 'hi', 60, 10]), target output: 80 (ignore any non-numerical
 // Given an array containing non-number values
 // When passed to the sum function
 // Then it should ignore the non-numerical values and return the sum of the numerical elements
+// Example usage
+
+describe("Apply sum to different criteria", () => {
+  test("given an empty array", () => {
+    expect(sum([])).toEqual(0);
+  });
+
+  test("Given an array with just one number", () => {
+    expect(sum([60])).toEqual(60);
+  });
+
+  test("given an array with numeric and non-numerical values", () => {
+    expect(sum(["Lesotho", -60, "Australia", 90])).toEqual(30);
+  });
+
+  test("given an array with positive and negative numbers", () => {
+    expect(sum([-30, -70, 500, 470])).toEqual(870);
+  });
+});
