aae279ea388f8f0f9862e243865ef7aa4900df0a
Syed Arslan <arslanhaider231@gmail.com>
2024-11-20 14:28:26 +0100

    complete implementation part of module js2 of week2

--- week-2/implement/contains.js
+++ week-2/implement/contains.js
@@ -1,3 +1,8 @@
-function contains() {}
+function contains(obj,key) {
+  if(typeof obj === "object" || obj === null || Array.isArray(obj) || typeof key !== "string"){
+    return false;
+  }
+  return true;
+}
 
 module.exports = contains;
--- week-2/implement/lookup.js
+++ week-2/implement/lookup.js
@@ -1,3 +1,11 @@
-function createLookup() {
-  // implementation here
+function createLookup(countryCurrencyPairs) {
+  const lookUp ={};
+  for (const [countryCode,CurrencyCode] of countryCurrencyPairs) {
+    lookUp[countryCode] = CurrencyCode;
+  }
+  return lookUp;
 }
+// const countryCurrencyPairs = [['US' , 'USD'], ['CA', 'CAD']];
+// const result = createLookup(countryCurrencyPairs);
+// console.log(result);
+module.exports = createLookup;
