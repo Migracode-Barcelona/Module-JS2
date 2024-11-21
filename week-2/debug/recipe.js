d6344734235e9f827b2ecb110877da3d7a857858
Priscilla Mac-Gatus <macgatuspriscilla@gmail.com>
2024-11-21 15:40:45 +0100

    Recipe debugged

--- week-2/debug/recipe.js
+++ week-2/debug/recipe.js
@@ -1,9 +1,3 @@
-// Predict and explain first...
-
-// This program should log out the title, how many it serves and the ingredients.
-// Each ingredient should be logged on a new line
-// How can you fix it?
-
 const recipe = {
   title: "bruschetta",
   serves: 2,
@@ -11,5 +5,5 @@ const recipe = {
 };
 
 console.log(`${recipe.title} serves ${recipe.serves}
-  ingredients:
-${recipe}`);
+Ingredients:
+${recipe.ingredients.join("\n")}`);
