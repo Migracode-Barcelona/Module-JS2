3d0c3165ad435eee99e3974595d9a1c25754c373
Syed Arslan <arslanhaider231@gmail.com>
2024-11-22 10:31:16 +0100

    change varibale name on sum.js

--- week-1/implement/sum.js
+++ week-1/implement/sum.js
@@ -1,7 +1,7 @@
 function sum(arr){
-    const nonNumerical = arr.filter(item => typeof item === "number" && !isNaN(item));
+    const numerical = arr.filter(item => typeof item === "number" && !isNaN(item));
      const sumAll = nonNumerical.reduce((a,c)=>a+c,0);
-    if(nonNumerical === 0){
+    if(numerical === 0){
         return null;
     }
     return sumAll;
