3b4c695982dec2c8adcddf770f1d87a057efb340
Afere precious <preciousafere@gmail.com>
2024-11-25 17:58:37 +0100

    author.js solution

--- week-2/debug/author.js
+++ week-2/debug/author.js
@@ -1,5 +1,8 @@
 // Predict and explain first...
-
+/**Answer:
+ * Javascript doesn't allow you to iterate over object value,
+ * objects don't have the natural sequence that arrays do
+ */
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
 
