524995dbeaa7eef72e99ff6cc4a84f3380e1cf5a
Syed Arslan <arslanhaider231@gmail.com>
2024-11-28 11:02:29 +0100

    fix the code of Quote-generator

--- week-3/quote-generator/quotes.js
+++ week-3/quote-generator/quotes.js
@@ -19,7 +19,12 @@
 function pickFromArray(choices) {
   return choices[Math.floor(Math.random() * choices.length)];
 }
+document.getElementById("new-quote").addEventListener("click",() =>{
+  const randomQuote = pickFromArray(quotes);
+  document.getElementById("quote").textContent = `"${randomQuote.quote}"`;
+  document.getElementById("author").textContent = `- ${randomQuote.author}`;
 
+})
 // A list of quotes you can use in your app.
 // DO NOT modify this array, otherwise the tests may break!
 const quotes = [
