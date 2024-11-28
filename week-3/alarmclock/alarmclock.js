47cfd9410c20dc7ecc34b4188428058e3f5da306
Syed Arslan <arslanhaider231@gmail.com>
2024-11-28 14:46:30 +0100

    fix the Alarmclock js code task

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,5 +1,32 @@
-function setAlarm() {}
-
+function setAlarm() {
+  const inputField = document.getElementById("alarmSet");
+  const alarmTime = parseInt(inputField.value, 10);
+  if(isNaN(alarmTime) || alarmTime<=0){
+    alert("Please enter a valid time in seconds.");
+  }
+  let timeRemaining = alarmTime
+  updateDisplay(timeRemaining);
+  const interval = setInterval(()=>{
+    if(timeRemaining > 0){
+      timeRemaining--;
+      updateDisplay(timeRemaining);
+    }else{
+      clearInterval(interval);
+      playAlarm();
+      document.body.style.backgroundColor = "red";
+    }
+  },1000);
+    document.getElementById("stop").addEventListener("click", () => {
+    clearInterval(interval);
+    pauseAlarm();
+    document.body.style.backgroundColor = ""; // Reset background color
+  });
+}
+function updateDisplay(seconds) {
+  const minute  = String(Math.floor(seconds/60)).padStart("2",0);
+  const second = String(seconds%60).padStart("2",0);
+  document.getElementById("timeRemaining").innerText = `Time Remaining: ${minute}:${second}`;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
