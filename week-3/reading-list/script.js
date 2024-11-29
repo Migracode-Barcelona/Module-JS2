5d58864c6965cf585ebb1f1719ba90da71b5f0c3
Afere precious <preciousafere@gmail.com>
2024-11-29 16:34:02 +0100

    Reading list

--- week-3/reading-list/script.js
+++ week-3/reading-list/script.js
@@ -20,4 +20,29 @@ const books = [
     bookCoverImage: "https://blackwells.co.uk/jacket/l/9780135957059.jpg",
   },
 ];
+function readingList(books) {
+  const bookListContainer = document.getElementById("reading-list");
+  books.forEach((book) => {
+    const itemList = document.createElement("li");
+    itemList.classList.add("book-item");
+    itemList.style.backgroundColor = book.alreadyRead ? "green" : "red";
 
+    const titleOfBooks = document.createElement("h3");
+    titleOfBooks.textContent = book.title;
+
+    const nameOfAuthor = document.createElement("p");
+    nameOfAuthor.textContent = `Author:${book.author}`;
+
+    const imageOfBooks = document.createElement("img");
+    imageOfBooks.src = book.bookCoverImage;
+    imageOfBooks.alt = book.title;
+    imageOfBooks.classList.add("book-cover");
+
+    itemList.appendChild(titleOfBooks);
+    itemList.appendChild(nameOfAuthor);
+    itemList.appendChild(imageOfBooks);
+
+    bookListContainer.appendChild(itemList);
+  });
+}
+readingList(books);
