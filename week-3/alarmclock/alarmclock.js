8e2f2e58bc8ccf15213c305180aa0a8ad8a99547
Syed Arslan <arslanhaider231@gmail.com>
2024-12-01 10:43:48 +0100

    improve design of my code

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,11 +1,12 @@
 function setAlarm() {
-  const inputField = document.getElementById("alarmSet");
-  const alarmTime = parseInt(inputField.value, 10);
+  const inputField = document.getElementById("alarmSet").value;
+  const alarmTime = parseInt(inputField,10);
   if(isNaN(alarmTime) || alarmTime<=0){
     alert("Please enter a valid time in seconds.");
   }
   let timeRemaining = alarmTime
   updateDisplay(timeRemaining);
+   const originalBackground = document.body.style.background;
   const interval = setInterval(()=>{
     if(timeRemaining > 0){
       timeRemaining--;
@@ -13,13 +14,15 @@ function setAlarm() {
     }else{
       clearInterval(interval);
       playAlarm();
-      document.body.style.backgroundColor = "red";
+     // document.body.style.backgroundColor = "red";
+     document.body.classList.add("alarm-active");
     }
   },1000);
     document.getElementById("stop").addEventListener("click", () => {
     clearInterval(interval);
     pauseAlarm();
-    document.body.style.backgroundColor = ""; // Reset background color
+   // document.body.style.backgroundColor = originalBackground; // Reset background color
+   document.body.classList.remove("alarm-active");
   });
 }
 function updateDisplay(seconds) {
