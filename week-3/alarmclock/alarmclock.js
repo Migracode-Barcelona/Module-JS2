8e6807481696efe1b569badc8f7c0183d16760c3
Afere precious <preciousafere@gmail.com>
2024-12-03 14:00:26 +0100

    Alarm-Clock-Exercise

--- week-3/alarmclock/alarmclock.js
+++ week-3/alarmclock/alarmclock.js
@@ -1,5 +1,33 @@
-function setAlarm() {}
+function setAlarm() {
+  const setTime = document.getElementById("timeRemaining");
+  const alarmSet = document.getElementById("alarmSet");
+  let countDown = parseInt(alarmSet.value);
 
+  if (isNaN(countDown)) {
+    alert("Input a valid Number");
+    return;
+  }
+
+  updateTimeCountDown(countDown, setTime);
+
+  const timer = setInterval(function () {
+    countDown--;
+
+    updateTimeCountDown(countDown, setTime);
+
+    if (countDown <= 0) {
+      clearInterval(timer);
+      playAlarm();
+    }
+  }, 1000);
+}
+function updateTimeCountDown(seconds, setTimeElement) {
+  const minutes = Math.floor(seconds / 60);
+  const secondsRemaining = seconds % 60;
+  setTimeElement.textContent = `Time Remaining ${minutes}:${secondsRemaining
+    .toString()
+    .padStart(2, "0")}`;
+}
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
