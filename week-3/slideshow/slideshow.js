aa9c7ac30fcfded2a39b59e7746648456d7cc520
Ly-Ly Janusz <153077816+lylyjan@users.noreply.github.com>
2024-12-07 13:04:27 +0100

    finished the slideshow

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -3,13 +3,26 @@ const images = [
     "./photos/image-2.jpg",
     "./photos/image-3.jpg",
     "./photos/image-4.jpg",
+    "./photos/image-5.jpg",
+    "./photos/image-6.jpg",
+    "./photos/image-7.jpg",
+    "./photos/image-8.jpg",
+    "./photos/image-9.jpg",
+    "./photos/image-10.jpg",
+    "./photos/image-11.jpg",
+
 
 ];
 
 let imageShown = document.getElementById("carousel-img")
 let buttonForward = document.getElementById("forward-btn")
 let buttonBackward= document.getElementById("backward-btn")
+let buttonAutoForward= document.getElementById("auto-forward-btn")
+let buttonAutoBackward= document.getElementById("auto-backward-btn")
+let buttonStop= document.getElementById("stop-btn")
+let slideshow
 
+//manual slideshow
 buttonForward.addEventListener('click', moveForward);
 
 let currentIndex = 0
@@ -25,16 +38,38 @@ function moveBackward(){
     imageShown.src = images[currentIndex];
 }
 
-        
+// automatic slideshow
+buttonAutoForward.addEventListener('click', slideshowForward);
+buttonAutoBackward.addEventListener('click', slideshowBackward);
+buttonStop.addEventListener('click', stopSlideshow);
 
+function slideshowForward() {
+    stopSlideshow()
+   slideshow= setInterval(function() { 
+        currentIndex = (currentIndex + 1) % images.length;
+    imageShown.src = images[currentIndex]
+     }, 3000)
+}
 
-// Write your code here
+function slideshowBackward() {
+    stopSlideshow()
+  slideshow=  setInterval(function() { 
+        currentIndex = (currentIndex -1 + images.length) % images.length;
+        imageShown.src = images[currentIndex];
+     }, 3000)
+}
+
+function stopSlideshow (){
+    clearInterval(slideshow)
+}
+
 // pseudocode 
 // first store the html id of the img and the two buttons in variables
 // then we need to add event listeners to buttons that listen for click
 // when they are clicked the image should Change 
 // and then execute a funciton which chnages the inner HTML of the given thingy
 
-// I might need this code later:  imageShown.innerHTML = `<img id="carousel-img" src= ${images[currentIndex]} alt="plant-pic" />`
+// I might need this code later:  imageShown.innerHTML = `<img id="carousel-img" src= ${images[currentIndex]} alt="vacation-pic" />`
+
 
 
