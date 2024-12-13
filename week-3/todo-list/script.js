d922a43b5b58ea38169271d05c0a1b304c998aa2
Ly-Ly Janusz <153077816+lylyjan@users.noreply.github.com>
2024-12-13 11:53:14 +0100

    moved todo-list from Ly-Ly to lyly-todo-list

--- week-3/todo-list/script.js
+++ week-3/todo-list/script.js
@@ -1,16 +1,31 @@
-function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
+let inputField = document.getElementById("input-field")
+let buttonAddTodo = document.getElementById("add-todo")
+let buttonDeleteLastTodo = document.getElementById("remove-last-todo")
+let buttonDeleteAllCompleted = document.getElementById("remove-all-completed")
+let list = document.getElementById("todo-list");
+
+buttonAddTodo.addEventListener("click", populateTodoList)
+
+function populateTodoList() {
+  let inputFieldValue  = inputField.value;
+  var newTodo = document.createElement('li');
+  newTodo.innerHTML = `- ${inputFieldValue}
+
+  `
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 }
 
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
-let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
-];
+// let todos = [
+//   { task: "Wash the dishes", completed: false },
+//   { task: "Do the shopping", completed: false },
+// ]; this is commented out because i do not really see the point of keeping this instead of using the inputfield, it was in the original CYF template but I took it out
 
-populateTodoList(todos);
+
+
+
+
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
