262176430b16b0f0f210e651283c3d885a665678
Afere precious <preciousafere@gmail.com>
2025-01-02 12:44:32 +0100

    silde-show level 1

--- week-3/slideshow/slideshow.js
+++ week-3/slideshow/slideshow.js
@@ -1,8 +1,28 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
-
 // Write your code here
+const imgElement = document.getElementById("carousel-img");
+const backwardElement = document.getElementById("backward-btn");
+const forwardElement = document.getElementById("forward-btn");
+
+let currentImage = 0;
+
+function imageUpdate() {
+  imgElement.src = images[currentImage];
+}
+function goBackward() {
+  currentImage = currentImage === 0 ? images.length - 1 : currentImage - 1;
+  imageUpdate();
+}
+function goForward() {
+  currentImage = currentImage === images.length - 1 ? 0 : currentImage + 1;
+  imageUpdate();
+}
+backwardElement.addEventListener("click", goBackward);
+forwardElement.addEventListener("click", goForward);
+
+imageUpdate();
