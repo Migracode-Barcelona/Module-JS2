a91e38f02118847eec16cbf9206ee2ac5358cdf7
Umair Riaz <umairriaz.ur2@gmail.com>
2025-01-16 03:56:46 +0100

    completed week 1

--- week-1/fix/median.js
+++ week-1/fix/median.js
@@ -3,10 +3,6 @@
 // If you're in the week-1 directory, you can run npm test -- fix to run the tests in the fix directory
 
 function calculateMedian(list) {
-  // const middleIndex = Math.floor(list.length / 2);
-  // const median = list.splice(middleIndex, 1)[0];
-  // return median;
-
   list.sort((a, b) => a - b);
   const middleIndex = Math.floor(list.length / 2);
 
