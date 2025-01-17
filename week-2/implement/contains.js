092814cdb48e19ed062d8f3e52cc1bf9b63a4809
Umair Riaz <umairriaz.ur2@gmail.com>
2025-01-17 01:49:51 +0100

    .

--- week-2/implement/contains.js
+++
@@ -1,9 +0,0 @@
-function contains(obj, prop) {
-  if (typeof obj !== "object" || obj === null || Array.isArray(obj)) {
-    return false;
-  }
-
-  return obj.hasOwnProperty(prop);
-}
-
-module.exports = contains;
